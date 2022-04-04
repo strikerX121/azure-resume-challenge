52673e3cb2571d55789b9d96c3eac27e3eb3954e
Antonio <antonio.cumberbatch2@gmail.com>
2022-04-04 18:37:26 -0400

    updated "using" list

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,7 +1,3 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
